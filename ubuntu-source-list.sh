6b050923e4c9278a60dfeac9e9690e7d8f675001
wangshengxing <worson.wang@aispeech.com>
2018-06-06 18:42:57 +0800

    environment adb

--- ubuntu-source-list.sh
+++ ubuntu-source-list.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sourcepath=~/project/config/shell-utills/
+
+#common
+source  $sourcepath/source-list.sh
+
 source  $sourcepath/ubuntu/app-list.sh
 source  $sourcepath/ubuntu/ev.sh
-
-#common 
-source  $sourcepath/source-list.sh
