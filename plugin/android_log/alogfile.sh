d5a38c06d90cecb911fd9ab0131632eb8e18ca58
wangshengxing <www.sheng.xing@gmail.com>
2018-06-08 22:41:45 +0800

    refactor root shell dir

--- plugin/android_log/alogfile.sh
+++ plugin/android_log/alogfile.sh
@@ -1,4 +1,7 @@
 t_file=~/Downloads/auto_log/log_auto_`date +%Y_%m_%d____%H_%M_%S`.txt
+if [  -n "$1" ]; then
+  t_file=~/Downloads/auto_log/$1_custom_`date +%Y_%m_%d____%H_%M_%S`.txt
+fi
 echo $t_file
 touch echo $t_file
 # sh adblogcat.sh $t_file
