3ee864762cb2f5d9cab9a0263d699939043d69b3
wangshengxing <www.sheng.xing@gmail.com>
2018-06-09 11:26:18 +0800

    add install sample

---
+++ sample/install/install_samba.sh
@@ -0,0 +1,13 @@
+# 安装
+
+sudo apt-get install samba
+sudo apt-get install smbclient
+
+
+# 配置
+# sudo gedit /etc/samba/smb.conf
+
+# 操作
+# 增加用户：sudo useradd smbuser
+# 设置用户密码：sudo smbpasswd -a smbuser"
+# 重启服务：sudo service smbd restart
---
+++ sample/install/install_zsh.sh
@@ -0,0 +1,8 @@
+#安装
+sudo apt-get install zsh
+
+#配置
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+
+#确认安装
+cat /etc/shells
