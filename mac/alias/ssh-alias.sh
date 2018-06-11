f020e8cb92bc6cca1a9bd31d023ce480bb5fea48
wangshengxing <www.sheng.xing@gmail.com>
2018-06-11 09:28:26 +0800

    add jira tools

--- mac/alias/ssh-alias.sh
+++ mac/alias/ssh-alias.sh
@@ -2,5 +2,5 @@ SSH_ADR="wangshengxing@172.16.153.235"
 export SSH_ADR
 
 alias ssh_connect="ssh $SSH_ADR"
-alias scp2="scp2.sh "
-alias scp4="sh scp4.sh "
+alias scp2="sh $CFG_PLUGIN_PATH/ssh_cmd/scp2.sh "
+alias scp4="sh $CFG_PLUGIN_PATH/ssh_cmd/scp4.sh "
