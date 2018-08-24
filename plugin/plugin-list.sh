ea2dd955655a73b8d3ec5fe283eb407fe55cac05
wangshengxing <www.sheng.xing@gmail.com>
2018-08-24 19:04:39 +0800

    加入android录屏功能

--- plugin/plugin-list.sh
+++ plugin/plugin-list.sh
@@ -13,6 +13,7 @@ source $CFG_PLUGIN_PATH/android_cmd/android-shot.sh
 #
 source $CFG_PLUGIN_PATH/android_log/log_utils.sh
 
+alias ascreen="sh $CFG_PLUGIN_PATH/android_screen/android_screen.sh"
 
 
 
@@ -20,3 +21,5 @@ G_CUSTOM_PATH="$CFG_PLUGIN_PATH/android_cmd"
 G_CUSTOM_PATH=$G_CUSTOM_PATH:"$PLUGIN_PATH/ssh_cmd"
 
 PATH=$PATH:$G_CUSTOM_PATH
+
+
